986511b9ef87db043b01e1a718ea238f33b56bbc
DADMIN <139256274+DADMIN1@users.noreply.github.com>
2023-12-18 14:56:09 -0900

    Update statsuckGUI.py
    simple fixes and improvements for the GUI

--- statsuckGUI.py
+++ statsuckGUI.py
@@ -1,32 +1,40 @@
 import tkinter as tk
-import time
-import csv
 from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
+
 import subprocess
 import pandas as pd
-from tkinter import scrolledtext
+import csv
 
-class NBAStatsApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("NBA Stats App")
+ 
+class TkApp(tk.Frame):
+    def __init__(self, master=None, title="NoTitle"):
+        super().__init__(master)
+        self.master.title(title)
+        self.grid()
 
         # Dropdown menu
         self.stats_var = tk.StringVar()
         self.stats_var.set("Select Stats Type")
-        self.stats_dropdown = ttk.Combobox(root, textvariable=self.stats_var,
+        self.stats_dropdown = ttk.Combobox(self.master, textvariable=self.stats_var,
                                            values=["Rebounding", "Passing"])
         self.stats_dropdown.grid(row=0, column=0, padx=10, pady=10)
-
+        
         # Fetch button
-        self.fetch_button = tk.Button(root, text="Fetch", command=self.fetch_stats)
+        self.fetch_button = tk.Button(self.master, text="Fetch", command=self.fetch_stats)
         self.fetch_button.grid(row=0, column=1, padx=10, pady=10)
 
         # Textbox to display data
-        self.data_textbox = scrolledtext.ScrolledText(root, width=200, height=52)
-        self.data_textbox.grid(row=1, column=3, columnspan=10, padx=30, pady=10)
+        self.data_textbox = ScrolledText(self.master)
+        self.data_textbox.grid(row=1, column=1, padx=10, pady=10)
 
+    # placeholder
     def fetch_stats(self):
+        print("pretending to fetch stats")
+        self.data_textbox.insert(tk.END, "legit stats:\n 1 2 3 4 5\n 69")
+    
+    # why is this even a member function?
+    def fetch_stats_real(self):
         stats_type = self.stats_var.get()
 
         if stats_type == "Rebounding":
@@ -35,7 +43,8 @@ class NBAStatsApp:
         elif stats_type == "Passing":
             subprocess.run(["bash", "AutoAssistFetch.bash"], check=True)
             csv_path = "file/path"
-        else:
+        else:  # invalid selection, somehow
+            print("stats_var has an invalid state")
             return
 
         # Read CSV file and display in the textbox
@@ -47,7 +56,8 @@ class NBAStatsApp:
             self.data_textbox.delete(1.0, tk.END)
             self.data_textbox.insert(tk.END, "No data available.")
 
+
 if __name__ == "__main__":
     root = tk.Tk()
-    app = NBAStatsApp(root)
-    root.mainloop()
+    NBAStatsApp = TkApp(root)
+    NBAStatsApp.mainloop()
