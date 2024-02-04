72c97e3e10ab9d202a6e2e035fef57f36e50e743
BicycleChrist <135588674+BicycleChrist@users.noreply.github.com>
2024-02-03 19:58:06 -0900

    moved team dict's segmented by league into this file big stuff

---
+++ Boddssuck/LeagueMap.py
@@ -0,0 +1,53 @@
+DEFAULT_LEAGUE_SELECT = "NBA"
+
+nhl_teams = [
+    'Anaheim Ducks', 'Arizona Coyotes', 'Boston Bruins', 'Buffalo Sabres',
+    'Calgary Flames', 'Carolina Hurricanes', 'Chicago Blackhawks',
+    'Colorado Avalanche', 'Columbus Blue Jackets', 'Dallas Stars', 'Detroit Red Wings',
+    'Edmonton Oilers', 'Florida Panthers', 'Los Angeles Kings', 'Minnesota Wild',
+    'Montreal Canadiens', 'Nashville Predators', 'New Jersey Devils', 'New York Islanders',
+    'New York Rangers', 'Ottawa Senators', 'Philadelphia Flyers', 'Pittsburgh Penguins',
+    'San Jose Sharks', 'Seattle Kraken', 'St. Louis Blues', 'Tampa Bay Lightning',
+    'Toronto Maple Leafs', 'Vegas Golden Knights', 'Washington Capitals',
+    'Winnipeg Jets', 'Vancouver Canucks'
+]
+
+nba_teams = {
+    "Atlanta Hawks": "ATL",
+    "Boston Celtics": "BOS",
+    "Brooklyn Nets": "BKN",
+    "Charlotte Hornets": "CHA",
+    "Chicago Bulls": "CHI",
+    "Cleveland Cavaliers": "CLE",
+    "Dallas Mavericks": "DAL",
+    "Denver Nuggets": "DEN",
+    "Detroit Pistons": "DET",
+    "Golden State Warriors": "GSW",
+    "Houston Rockets": "HOU",
+    "Indiana Pacers": "IND",
+    "Los Angeles Clippers": "LAC",
+    "Los Angeles Lakers": "LAL",
+    "Memphis Grizzlies": "MEM",
+    "Miami Heat": "MIA",
+    "Milwaukee Bucks": "MIL",
+    "Minnesota Timberwolves": "MIN",
+    "New Orleans Pelicans": "NOP",
+    "New York Knicks": "NYK",
+    "Oklahoma City Thunder": "OKC",
+    "Orlando Magic": "ORL",
+    "Philadelphia 76ers": "PHI",
+    "Phoenix Suns": "PHX",
+    "Portland Trail Blazers": "POR",
+    "Sacramento Kings": "SAC",
+    "San Antonio Spurs": "SAS",
+    "Toronto Raptors": "TOR",
+    "Utah Jazz": "UTA",
+    "Washington Wizards": "WAS"
+}
+
+leaguemap = {
+    "NHL": nhl_teams,
+    "NBA": nba_teams.keys(),
+}
+
+
