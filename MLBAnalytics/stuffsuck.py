31e4f9c8eee1bcb127e9d259d3cdbea106653cf4
BicycleChrist <135588674+BicycleChrist@users.noreply.github.com>
2024-05-08 02:21:37 -0800

    Updated script to use batballsucks logic and not use selenium lol

--- MLBAnalytics/stuffsuck.py
+++ MLBAnalytics/stuffsuck.py
@@ -1,84 +1,72 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-import pandas as pd
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import requests
+import pathlib
+import pandas
 import time
 
 
-# ganked the selenium options/imports from boundFetch
-# every page but the stuff+ table is very inconsistent
-# Thought we could get away w/ just BS4 and req but no dice
 
-#TODO:save html files to own folder along with data files
-
-def download_html_selenium(url, filename):
-    service = FirefoxService()
-    options = FirefoxOptions()
-    options.add_argument('-headless')  # Uncomment if you run in headless mode
-    options.add_argument("--window-size=1920x,1080")
-    driver = webdriver.Firefox(service=service, options=options)
-
-
-    driver.get(url)
-
-    # Get page source
-    html_content = driver.page_source
-
-    # Write html to file
-    with open(filename, 'w') as file:
-        file.write(html_content)
-
-    # Close WebDriver
-    driver.quit()
-
-    print(f"HTML file downloaded successfully as '{filename}'.")
-    #
 
 # Extract tableski data from HTML
-def extract_table_data(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    table_rows = soup.find_all('tr', class_='is-selected__invalid')
+def extract_table_data(soup):
+    toplevel = soup.find('div', class_="fg-data-grid table-type").extract()
+    bottomlevel = toplevel.find('div', class_='table-wrapper-inner').extract()
+    dataframes = pandas.read_html(bottomlevel.encode())  # list of two tables; 'table-scroll' and 'table-fixed'(hidden)
+    # they should be the same table, probably
+    return dataframes
 
-    data = []
-    for row in table_rows:
-        cols = row.find_all('td')
-        cols_data = {}
-        for col in cols:
-            col_id = col.get('data-col-id')  # Use .get() method to handle missing attributes
-            col_data = col.text.strip()
-            if col_id:
-                cols_data[col_id] = col_data
-        data.append(cols_data)
-
-    return data
-
-# convert table data to CSV
-def convert_to_csv(table_data, filename):
-    df = pd.DataFrame(table_data)
-    current_time = time.strftime("%Y-%m-%d %H-%M-%S")  # Get current date and time
-    new_filename = f"{filename[:-4]}_{current_time}.csv"  # Append date and time to filename
-    df.to_csv(new_filename, index=False)
-    print(f"Table data saved to '{new_filename}'.")
+    #tables = bottomlevel.contents
+    #print([table.attrs['class'] for table in tables])
+    ##tablestwo = {'table-scroll': bottomlevel.find('div', class_='table'), 'table-fixed': bottomlevel.find('div', class_='table-fixed')}
+    #print("plzbreak")
+    #table_scroll = tables[0]
+    ## pandas.read_html always returns a list of dataframes (even if only one is returned)
+    #dataframe = pandas.read_html(table_scroll.encode())[0]
+    ##dataframes = [pandas.read_html(table.encode()) for table in tables]
+    #return
 
 
-def main():
-    url = 'https://www.fangraphs.com/leaders/major-league?type=36&pos=all&stats=pit&sortcol=3&sortdir=default&qual=1&pagenum=1&pageitems=2000000000'
-    html_file = 'FG.html'
-    csv_file = 'stuffplustable_data.csv'
+def LoadHTMLfile(file_name):
+    cwd = pathlib.Path().cwd()
+    html_file_path = cwd / file_name
+    with open(html_file_path, encoding="utf-8", mode='r') as html_file:
+        newsoup = BeautifulSoup(html_file.read(), 'lxml')
+    return newsoup
 
-    # Download HTML file
-    download_html_selenium(url, html_file)
 
-    # Extract table data
-    with open(html_file, 'r') as file:
-        html_content = file.read()
-        table_data = extract_table_data(html_content)
-        convert_to_csv(table_data, csv_file)
-        print("Table extracted and saved successfully.")
+def DownloadHTMLfile(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("did not return 200")
+        return None
+    newsoup = BeautifulSoup(response.content, 'lxml').extract()
+    return newsoup
+
+
+def SaveDataframe(dataframe, file_name):
+    cwd = pathlib.Path().cwd()
+    save_dir = cwd / "MLBstats"
+    current_time = time.localtime()
+    timestr = str(current_time.tm_hour) + str(current_time.tm_min) + str(current_time.tm_sec)
+    file_name = file_name + timestr + '.csv'
+    dump_path = save_dir / file_name
+    dataframe.to_csv(dump_path)
+    return dump_path
+
 
 if __name__ == "__main__":
-    main()
+    # Minimum number of at bats can be changed via the "qual=" option at end of URL
+    url = 'https://www.fangraphs.com/leaders/major-league?type=36&pos=all&stats=pit&sortcol=3&sortdir=default&qual=1&pagenum=1&pageitems=2000000000'
+    #soup = LoadHTMLfile('FGhitter.html')
+    newsoup = DownloadHTMLfile(url)
+    dataframes = extract_table_data(newsoup)
+    SaveDataframe(dataframes[0], "stuffplustable_data_")
+
+    print("pls dont exit")
+
+
+
+
+
+
