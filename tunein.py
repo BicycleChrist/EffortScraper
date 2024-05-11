a0c687fd91f839abf6bd10b789b742a411597cc7
BicycleChrist <135588674+BicycleChrist@users.noreply.github.com>
2024-05-10 16:48:27 -0800

    Bigly Improved tunein.py

--- tunein.py
+++ tunein.py
@@ -5,13 +5,43 @@ import threading
 # TODO: Ignore links that don't lead to a stream, output is far greater than needed
 # I.E https://buffstreams.ai/soccer or https://the.streameast.app/team/anaheim-ducks
 
+# add methods for new sites in here
+def GetSiteMainContent(site, soup:BeautifulSoup):
+    if site == "buffstreams":
+        main_content = soup.find('main', class_="container contentContain")
+        return main_content.extract()
+    elif site == "streameast": # hoping that HTML is the same across mirrors
+        site_wrapper = soup.find('div', class_="site-wrapper")
+        main_content = site_wrapper.find('main')
+        return main_content.extract()
+    else:
+        print("unrecognized site")
+        return soup
+    return soup
+
+
+def GetSite(url):
+    start_name = url.removeprefix("https://")
+    if start_name.startswith("buffstreams"): return "buffstreams"
+    elif start_name.startswith("the.streameast"): return "streameast"
+    else: return "unknown site"
+
+def SiteFilterHref(site, link):
+    lastsegment = link.split('/')[-1]
+    if not lastsegment.isnumeric(): return False
+    if site == "streameast":
+        if '/team/' in link: return False
+    return True
 
 def get_href_links(url):
     try:
-        response = requests.get(url)
+        # TODO: check status code
+        response = requests.get(url, timeout=5)
         soup = BeautifulSoup(response.content, 'html.parser')
-        links = soup.find_all('a', href=True)
-        href_links = [link['href'] for link in links]
+        site = GetSite(url)
+        main_content = GetSiteMainContent(site, soup)
+        links = main_content.find_all('a', href=True)
+        href_links = [link['href'] for link in links if SiteFilterHref(site, link['href'])]
         return href_links
     except Exception as e:
         print(f"Error fetching links from {url}: {e}")
@@ -39,9 +69,9 @@ def parse_urls(urls):
 if __name__ == "__main__":
     urls = [
         "https://the.streameast.app/nhlstreams3",
-        #"https://the.streameast.app/mlbstreams17",
-        #"https://the.streameast.app/nbastreams64",
-        #"https://the.streameast.app/nflstreams3",
+        "https://the.streameast.app/mlbstreams17",
+        "https://the.streameast.app/nbastreams64",
+        "https://the.streameast.app/nflstreams3",
         "https://buffstreams.ai/mlb",
         "https://buffstreams.ai/nba",
         "https://buffstreams.ai/nhl"
