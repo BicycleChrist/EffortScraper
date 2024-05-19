e4e04fc73dd4c3b9bc2152cf42d95903e0125e29
DADMIN1 <139256274+DADMIN1@users.noreply.github.com>
2024-05-18 22:36:19 -0800

    Improved update_importpaths.py: Made relative-path construction Windows-compatible Significantly improved validation of symlink targets Added (optional, nondefault) overwriting behavior (changes merged from ImportTest)

--- update_importpaths.py
+++ update_importpaths.py
@@ -1,5 +1,5 @@
-import sys
 import pathlib
+import sys
 
 
 # name of the project folder
@@ -30,7 +30,7 @@ def FindToplevelPath() -> pathlib.Path:
                 return parent
         assert False, "Unreachable"
     else:
-        print(f"toplevel directory: {toplevel_name} is not a parent of current directory")
+        print(f"toplevel directory: '{toplevel_name}' is not a parent of current directory")
         raise ImportError
     assert False, "Unreachable"
 
@@ -43,46 +43,82 @@ def Update_ImportPaths(onlyGood=False):
     bad_paths  = [ path.name for path in unchecked_paths if not path.exists() or not path.is_dir() ]
     if len(bad_paths) > 0:
         for name in bad_paths:
-            print(f"error: '{name}' does not exist or is not a directory")
+            print(f"ERROR: '{name}' does not exist or is not a directory")
         raise ImportError
     if onlyGood: return good_paths
     return [ str(toplevel_path), *good_paths, *sys.path]
 
 
 # 'relative_to' doesn't accept 'walk_up' until 3.12
-def CreateRelativePath(frompath: pathlib.Path, topath: pathlib.Path, target_is_dir=False):
+def CreateRelativePath(frompath: pathlib.PurePath, topath: pathlib.PurePath, target_is_dir=False):
+    toplevel_path = FindToplevelPath()
     target_parents = []
-    if target_is_dir: target_parents.append(topath)
-    target_parents.extend(topath.parents)
+    if target_is_dir and topath.is_relative_to(toplevel_path): 
+        target_parents.append(topath)
     
-    relative_string = ""
-    for parent in frompath.parents:
-        if parent in target_parents:
-            if target_is_dir:
-                relative_string = relative_string + parent.name + "/"
-            relative_string = relative_string + topath.name
-            return relative_string
-        relative_string = relative_string + "../"
+    topath_parents_filtered = [p for p in topath.parents if p.is_relative_to(toplevel_path)]
+    target_parents.extend(topath_parents_filtered)
+    
+    # Simply assigning 'frompath' to 'relative_path' doesn't work; that creates an absolute path
+    relative_path = frompath.relative_to(frompath)
+    frompath_parents_filtered = [p for p in frompath.parents if p.is_relative_to(toplevel_path)]
+    for parent in frompath_parents_filtered:
+        if parent in target_parents:  # TODO: handle the case where the target is a dir?
+            relative_path = relative_path.joinpath(topath.name)
+            return relative_path
+        relative_path = relative_path.joinpath('..')
+    
     print("error: paths had no common ancestor")
     return None
 
 
-def CreateSymlinks():
+# returns a list of newly-written symlinks (symlink_location, relativePath)
+def CreateSymlinks(report_existing=True, overwrite=False) \
+    -> list[tuple[pathlib.Path, pathlib.PurePath]]:
+    toplevel_path = FindToplevelPath()
     thisfile = pathlib.Path(__file__)  # __file__ returns absolute path, by the way
     assert thisfile.exists(), "how are you running this??"
     # Update_ImportPaths returns a list of strings (for sys.path), so we convert back to pathlib.Path
-    good_paths = [pathlib.Path(path) for path in Update_ImportPaths(True)]
+    good_paths = [pathlib.Path(path) for path in Update_ImportPaths(onlyGood=True)]
     target_dirs = [path for path in good_paths if path.is_dir() and path.name in symlink_into]
     
+    # tuples are: (symlink_location, relativePath)
+    written_symlinks: list[tuple[pathlib.Path, pathlib.PurePath]] = []
     for subdirectory in target_dirs:
         symlink_location = subdirectory / thisfile.name
-        if symlink_location.exists(): continue
-        #symlink_location.symlink_to(thisfile)
+        assert (symlink_location != thisfile), "ERROR: symlink would overwrite this file!"
+        # assert (not thisfile.samefile(symlink_location))  # can't do this because it resolves the symlink and the assert fails (if symlink target is correct)
+        if symlink_location.exists():
+            # before overwrite, verify that the pre-existing file is a symlink (and assume it belongs to this script)
+            # we don't use 'samefile()' to compare them, because we can't assume the old symlink's target is correct
+            if not symlink_location.is_symlink():
+                print(f"ERROR: file already exists at: '{symlink_location.relative_to(toplevel_path)}', but it is NOT a symlink!")
+                if overwrite:
+                    moved_location = f"{symlink_location.with_suffix('.backup')}"
+                    symlink_location.rename(moved_location)
+                    print(f"pre-existing (non-symlink) file moved to: {moved_location}")
+                else: print(f"WARNING: '{symlink_location}' will NOT be overwitten (not a symlink); skipping"); continue;
+            # if pre-existing file IS a symlink 
+            elif report_existing: print(f"existing symlink found: '{symlink_location.relative_to(toplevel_path)}'")
+            if overwrite: print(f"OVERWRITING '{symlink_location}'"); symlink_location.unlink(missing_ok=True);
+            else: continue  # fallthrough if 'overwrite'
+        # note: symlink_location must NOT exist after this point; otherwise 'symlink_to' will fail
+        # 'symlink_to' does not have any sort of "missing_ok" argument
+        
         relativePath = CreateRelativePath(symlink_location, thisfile)
-        if relativePath is None: print("failed creating symlink!"); continue
-        symlink_location.symlink_to(relativePath)
-        print(f"symlink created at: {symlink_location}")
-    return
+        if relativePath is None: print("failed to construct relativePath while creating symlink!"); continue
+        resolved_path = (subdirectory / relativePath).resolve()
+        if resolved_path.is_relative_to(toplevel_path):
+            symlink_location.symlink_to(relativePath)
+            print(f"symlink created at: '{symlink_location}'")
+            written_symlinks.append((symlink_location, relativePath))
+            # apparently the 'target_is_directory' argument for "symlink_to()" only has an effect on Windows?
+        else:
+            print(f"ERROR: invalid symlink (outside toplevel): '{resolved_path}'")
+            print(f"\t created when resolving relative-path: '{relativePath}' \n\t from subdirectory: '{subdirectory}'\n")
+            print("CreateRelativePath() must be bugged. Please fix it.")
+            break
+    return written_symlinks
 
 
 if __name__ == "__main__":
@@ -90,6 +126,7 @@ if __name__ == "__main__":
     newpaths = Update_ImportPaths()
     print(f"updated sys.path:\n {newpaths} \n")
     CreateSymlinks()
+    #CreateSymlinks(overwrite=True)
     exit(0)
 
 
