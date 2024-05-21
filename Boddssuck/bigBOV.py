9ba820fc0b5937e2f72af042e8e45f9d30b2d375
BicycleChrist <135588674+BicycleChrist@users.noreply.github.com>
2024-05-21 01:08:50 -0800

    Handeling for no game links being found

--- Boddssuck/bigBOV.py
+++ Boddssuck/bigBOV.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import *
 from time import sleep  # TODO: replace sleeps with selenium expected-conditions
 
 
+
 def TryToFind(element, xpath_string):
     try:
         x = element.find_element(By.XPATH, xpath_string)
@@ -21,19 +22,21 @@ def TryToFind(element, xpath_string):
 
 
 def FindGameLinks(driver: webdriver.Firefox):
-    url = 'https://www.bovada.lv/sports/baseball/mlb'
+    url = 'https://www.bovada.lv/sports/politics'
+    #url = 'https://www.bovada.lv/sports/baseball/mlb'
     driver.get(url)
-    sleep(3)
-    #wait = WebDriverWait(driver, 5)
-    #next_events_bucket = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'next-events-bucket')))
-    #grouped_events = next_events_bucket.find_elements(By.CLASS_NAME, 'grouped-events')
-    sp_path_event = driver.find_element(By.XPATH, "/html/body//div[@class='top-container']//main/div[@class='sp-main-area']//sp-path-event")
-    grouped_events = sp_path_event.find_elements(By.XPATH, ".//div[@class='grouped-events']")
-    
+    sleep(1)
+    try:
+        sp_path_event = driver.find_element(By.XPATH, "/html/body//div[@class='top-container']//main/div[@class='sp-main-area']//sp-path-event")
+        grouped_events = sp_path_event.find_elements(By.XPATH, ".//div[@class='grouped-events']")
+    except NoSuchElementException:
+        print("No grouped events found on the page.")
+        return []
+
     events = []
     for group in grouped_events:
         events.extend(group.find_elements(By.XPATH, ".//sp-coupon//a[@class='game-view-cta']"))
-    
+
     game_links = [event.get_attribute('href') for event in events]
     print(f"\n game_links: {game_links} \n")
     return game_links
@@ -44,22 +47,18 @@ def VisitGameLink(driver:webdriver.Firefox, game_link):
     driver.get(game_link)
     sleep(1)
     print("got link")
-    # Wait until the sp-main-area element is loaded
-    #main_area = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'sp-main-area')))
     main_area = driver.find_element(By.CLASS_NAME, 'sp-main-area')
     print("found main area")
     event = main_area.find_element(By.XPATH, './/sp-event')
     print("found event")
 
-    #  event-coupons within event-info incredible
     event_coupons = event.find_elements(By.TAG_NAME, 'sp-coupon')
     print("found event coupons")
-    # this part is really slow for some reason; seems to be triggering the implicit wait a lot
     article_lists = [coupon.find_elements(By.TAG_NAME, "article") for coupon in event_coupons]
-    intermediate = [a for a in article_lists if len(a) > 0]  # I think this is necessary
+    intermediate = [a for a in article_lists if len(a) > 0]
     article_lists = intermediate
     print(article_lists)
-    
+
     results = []
     for articlelist in article_lists:
         for article in articlelist:
@@ -68,36 +67,63 @@ def VisitGameLink(driver:webdriver.Firefox, game_link):
                 new_dict[thing] = article.find_element(By.TAG_NAME, thing).text.splitlines()
             results.append(new_dict)
             print(new_dict)
-    
+
     print(results)
     return results
 
+
+# For futures markets,(I.E politics and league championship winners), odds are contained on the initial page
+# In the event that no game links are found for the given url, we'll parse the current page
+def ScrapeCurrentPage(driver:webdriver.Firefox):
+    print("no game links found, scraping current page")
+    main_area = driver.find_element(By.CLASS_NAME, 'sp-main-area')
+    print("found main area")
+    event = main_area.find_element(By.CLASS_NAME, 'max-container')
+    print("found event")
+
+    event_coupons = event.find_elements(By.TAG_NAME, 'sp-coupon')
+    print("found event coupons")
+    article_lists = [coupon.find_elements(By.TAG_NAME, "article") for coupon in event_coupons]
+    intermediate = [a for a in article_lists if len(a) > 0]
+    article_lists = intermediate
+    print(article_lists)
+
+    results = []
+    for articlelist in article_lists:
+        for article in articlelist:
+            new_dict = {}
+            for thing in ('header', 'section'):
+                new_dict[thing] = article.find_element(By.TAG_NAME, thing).text.splitlines()
+            results.append(new_dict)
+            print(new_dict)
+
+    print(results)
+    return results
+
+
 if __name__ == "__main__":
-    #TODO: Acquire live odds; pages are able to be parsed quite fast for bigly suck up
-    #TODO : Figure out name of header element for odds containers
     options = FirefoxOptions()
-    #options.add_argument('--headless')
-    
     options.page_load_strategy = 'eager'
+    options.add_argument('--headless')
     firefox_profile = FirefoxProfile()
-    #firefox_profile.set_preference("permissions.default.image", 2)
     options.profile = firefox_profile
     print(firefox_profile)
     print(options.profile.path)
-    
-    #options.add_argument('-headless')
+
     driver = webdriver.Firefox(options=options)
     driver.implicitly_wait(1)
     print("finding game links")
     game_links = FindGameLinks(driver)
-    # game_links = ["https://www.bovada.lv/sports/hockey/united-states/ahl/coachella-valley-firebirds-ontario-reign-202405192200"]
-    for game_link in game_links:
-        try:
-            VisitGameLink(driver, game_link)
-        except StaleElementReferenceException as stale_exception:
-            print(f"selenium is tarding out: {stale_exception}")
-            continue
-    
+
+    if game_links:
+        for game_link in game_links:
+            try:
+                VisitGameLink(driver, game_link)
+            except StaleElementReferenceException as stale_exception:
+                print(f"selenium is tarding out: {stale_exception}")
+                continue
+    else:
+        ScrapeCurrentPage(driver)
+
     print("done")
     driver.quit()
-    
