0d855361cb676cd17e457a3ffdf1145d804aaab1
DADMIN1 <139256274+dadmin1@users.noreply.github.com>
2024-06-19 03:17:54 -0800

    Huge improvement to PinnaclePlz(poolski)

--- Boddssuck/PinnaclePlzpoolski.py
+++ Boddssuck/PinnaclePlzpoolski.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.remote.webdriver import WebElement
 from selenium.common.exceptions import *
 from time import sleep
 import pathlib
 import pprint
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 #TODO Let Paul C know he should have tried harder.
 #TODO Scrape the page faster, threading isnt enough ):
@@ -28,8 +29,21 @@ URLbySport = {
     "NBA": ("basketball/nba", "#player-props"),
     "NHL": ("hockey/nhl",     "#game-props"),
     "NFL": ("football/nfl",   ""),
-    "MLB": ("baseball/mlb",   ""),
+    "MLB": ("baseball/mlb",   "#player-props"),
 }
+
+
+def TryToFind(element:WebElement, locator_string:str, locator_method=By.XPATH, print_onfail=False, find_multi=False) -> WebElement | list[WebElement] | None:
+    try:
+        copy_element = element
+        if find_multi: return copy_element.find_elements(locator_method, locator_string)
+        return copy_element.find_element(locator_method, locator_string)
+    except NoSuchElementException:
+        if print_onfail:
+            print(f"error trying to find: {locator_string}, with method: {locator_method}")
+            #print(f"from: {element}")
+    return None
+
 
 def FindGameLinks(driver, sport):
     urlseg, urlsuffix = URLbySport[sport]
@@ -86,7 +100,6 @@ def VisitPage(url, driver: webdriver.Firefox, sport):
         # check if we're on a 'Matchup not found.' page (this might be a bad idea)
         noEventsBlock = driver.find_element(By.XPATH, "//div[contains(@class, 'noEvents')][@data-test-id='NoEvents-Container']")
         print(noEventsBlock.text)
-        print("Matchup not found")
         return driver
     try:
         print("finding oddsFormatDropdown...")
@@ -112,86 +125,178 @@ def VisitPage(url, driver: webdriver.Firefox, sport):
     return driver
 
 
+# contentblock is the child div under the marketgroup div with the class 'collapse-content'
+def SubdivideMarketGroup(contentblock: WebElement) -> dict:
+    def Findlambda(locator_string:str, locator_method=By.XPATH, element=contentblock, find_multi=False):
+        return TryToFind(element, locator_string, locator_method, find_multi=find_multi)
+    
+    webelements = {
+        "subheadings"    : Findlambda("./ul/li",   find_multi=True),            # pretty sure this is the only element with this tag
+        "buttonrows"     : lambda: Findlambda("./div/div", find_multi=True),    # in the HTML they're 'buttonRow', but they're grouped by column # Maybe delete this comment  
+        "expandMarketBtn": Findlambda("./button"),                                          # the expand button is the only one directly nested under the g
+    }
+    
+    return webelements
+
+
+# webelements is supposed to be the dict reeturned from SubdivideMarketGroup
+def ParseMarketGroup(webelements: dict) -> dict:
+    subheadings, buttonrows, expand_button = webelements.values()
+    if expand_button is not None:
+        if expand_button.text == "See more":
+            expand_button.click(); #print("click")
+            # TODO: figure out if clicking the expand button invalidates the buttoncolumns
+    
+    def ParseButton(button: WebElement) -> dict:
+        return {button.get_attribute("title"): int(button.get_attribute("aria-label").removeprefix("Money Line "))}
+    
+    index = 0
+    def GetNextIndex():
+        nonlocal index; index += 1
+        return index
+    
+    buttonrow_buttons = {
+        GetNextIndex(): buttonrow.find_elements(By.XPATH, './div/button') for index, buttonrow in enumerate(buttonrows())
+    }
+    
+    # for testing
+    #subheadings = ("Houston Astros", "Chicago White Sox")
+    #buttonrow_buttons[1] = list(reversed(buttonrow_buttons[0]))
+    
+    if len(subheadings) > 0:
+        column_headers = [subheading.text for subheading in subheadings]
+        return {
+            column_headers[localindex%2] : { key:value }
+            for buttonlist in buttonrow_buttons.values()
+            for localindex, button in enumerate(buttonlist)
+            for key, value in ParseButton(button).items()
+        }
+    
+    # no subheading
+    return {
+        key: value
+        for buttonlist in buttonrow_buttons.values()
+        for button in buttonlist
+        for key, value in ParseButton(button).items()
+    }
+# end of ParseMarketGroup
+
+
+def SearchButDontScrewMe(element, *args):
+    return (lambda copyelement=element: copyelement.find_element(*args))()
+
+
 def ScrapePage(driver: webdriver.Firefox):
     market_dict = {}
-    try:
-        matchup_market_groups = driver.find_element(By.XPATH, ".//div[contains(@class, 'matchup-market-groups')]")  # container for all the elements of the stats
-    except Exception as E:
+    html_body = driver.find_element(By.XPATH, '/html/body')
+    matchup_market_groups = TryToFind(html_body, ".//div[contains(@class, 'matchup-market-groups')]")
+    if matchup_market_groups is None:
         print("ScrapePage failed to find matchup-market-groups")
-        print(E)
         return
+    
     driver.implicitly_wait(0)
-    #market_groups = matchup_market_groups.find_elements(By.XPATH, './/div[@data-test-id="Collapse"][@data-collapsed="false"]')
-    market_groups = matchup_market_groups.find_elements(By.XPATH, './/div[@data-test-id="Collapse"]')
-    for group in market_groups:
-        if group.get_attribute("data-collapsed") == "true":
-            print("unexpanded element!!!")
-            continue
-        # TODO: on some pages ('#All'), the market-group content blocks will have some of their content hidden, with a clickable 'See more' footer (even if you've hit 'Show All')
-        # Assuming there's only one title and content group per market-group
-        driver.implicitly_wait(1)
-        title = group.find_element(By.XPATH, ".//div[contains(@class, 'collapse-title')]").text.removesuffix('\nHide All')  # the 'Show/Hide All' button text will also be concatenated, if it exists
-        content = group.find_element(By.XPATH, ".//div[contains(@class, 'collapse-content')]")
-        try:
-            expandMarketBtn = content.find_element(By.XPATH, "./button")  # the expand button is the only one directly nested under the group div
-            print(f"Found expandMarketBtn: {expandMarketBtn.text}")
-            if expandMarketBtn.text == "See more":
-                expandMarketBtn.click()
-                print("click")
-                assert(expandMarketBtn.text == "See less")
-        except NoSuchElementException:  # group doesn't have any hidden elements
-            pass
+    market_groups = matchup_market_groups.find_elements(By.XPATH, './/div[@data-test-id="Collapse"][@data-collapsed="false"]')  # filtering out collapsed elements (maybe a bad idea)
+    #market_groups = matchup_market_groups.find_elements(By.XPATH, './/div[@data-test-id="Collapse"]')
+    driver.implicitly_wait(1)
+    
+    market_blocks = {
+        # title : content (container for buttons and subheading, if any)
+        SearchButDontScrewMe(group, By.XPATH, "./div[contains(@class, 'collapse-title')]").text.removesuffix('\nHide All') :
+        SearchButDontScrewMe(group, By.XPATH, "./div[contains(@class, 'collapse-content')]")
+        for group in market_groups
+    }
+    
+    for title, block in market_blocks.items():
         print(title)
-        try:  # find subHeading if it exists (only present on Live-Odds pages?)
-            subHeading = content.find_element(By.XPATH, "./ul[li and li]")  # ul with two child elements tagged li (which usually contain the team names)
-            print(subHeading.text)
-        except NoSuchElementException:
-            subHeading = None
-        driver.implicitly_wait(0)
-        market_dict[title] = {"subHeading": subHeading, "content": []}
-        market_buttons = content.find_elements(By.XPATH, ".//button[contains(@class, 'market-btn')]")
-        for btn in market_buttons:
-            temptxt = btn.text
-            if len(temptxt) > 0:
-                market_dict[title]["content"].append(btn.text.splitlines())
-                print(temptxt)
-            else:
-                market_dict[title]["content"].append("Line Closed or Not Posted")
-                print("Line Closed or Not Posted")
-        print("")
-        driver.implicitly_wait(1)
+        group_contents = SubdivideMarketGroup(block)
+        result = ParseMarketGroup(group_contents)
+        market_dict[title] = result
+    
     return market_dict
+
+# 
+# def Temp():
+#     market_dict = {}
+#     market_groups = []
+#     for group in market_groups:
+#         if group.get_attribute("data-collapsed") == "true":
+#             print("unexpanded element!!!")
+#             continue
+#         # TODO: on some pages ('#All'), the market-group content blocks will have some of their content hidden, with a clickable 'See more' footer (even if you've hit 'Show All')
+#         # Assuming there's only one title and content group per market-group
+#         driver.implicitly_wait(1)
+#         
+#         title = group.find_element(By.XPATH, ".//div[contains(@class, 'collapse-title')]").text.removesuffix('\nHide All')  # the 'Show/Hide All' button text will also be concatenated, if it exists
+#         content = group.find_element(By.XPATH, ".//div[contains(@class, 'collapse-content')]")
+#         
+#         expandMarketBtn = []
+#         for button in expandMarketBtn:
+#             expandMarketBtn = content.find_element(By.XPATH, "./button")  # the expand button is the only one directly nested under the group div
+#             print(f"Found expandMarketBtn: {expandMarketBtn.text}")
+#             if expandMarketBtn.text == "See more":
+#                 expandMarketBtn.click()
+#                 print("click")
+#                 assert(expandMarketBtn.text == "See less")
+#         print(title)
+#         try:  # find subHeading if it exists (only present on Live-Odds pages?)
+#             subHeading = content.find_element(By.XPATH, "./ul[li and li]")  # ul with two child elements tagged li (which usually contain the team names)
+#             print(subHeading.text)
+#         except NoSuchElementException:
+#             subHeading = None
+#         driver.implicitly_wait(0)
+#         market_dict[title] = {"subHeading": subHeading, "content": []}
+#         market_buttons = content.find_elements(By.XPATH, ".//button[contains(@class, 'market-btn')]")
+#         for btn in market_buttons:
+#             temptxt = btn.text
+#             if len(temptxt) > 0:
+#                 market_dict[title]["content"].append(btn.text.splitlines())
+#                 print(temptxt)
+#             else:
+#                 market_dict[title]["content"].append("Line Closed or Not Posted")
+#                 print("Line Closed or Not Posted")
+#         print("")
+#         driver.implicitly_wait(1)
+#     return market_dict
+
 
 # Initialize WebDriver for each thread
 def initialize_driver():
     options = FirefoxOptions()
     options.add_argument('--headless')
+    #options.page_load_strategy = 'eager'  # breaks everything
+    
     #firefox_profile = FirefoxProfile(profile_directory=ProfilePath())
     #options.profile = firefox_profile
     driver = webdriver.Firefox(options=options)
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(1)
     return driver
 
 def scrape_link(link, sport):
+    print(f"scraping {link}")
     driver = initialize_driver()
-    current_state = VisitPage(link, driver, sport)
-    market_data = ScrapePage(current_state)
-    driver.quit()
+    try:
+        current_state = VisitPage(link, driver, sport)
+        market_data = ScrapePage(current_state)
+    finally:
+        driver.quit()
     return market_data
+
 
 if __name__ == "__main__":
     cwd = pathlib.Path.cwd()
     assert (cwd.name == "Boddssuck" and "you're in the wrong directory")
     default_sport = "MLB"
-
+    
     driver = initialize_driver()
     with driver.context(driver.CONTEXT_CONTENT):
         links = FindGameLinks(driver, default_sport)
     driver.quit()
-
-
+    
+    links = links[0:3]
+    
     with ThreadPoolExecutor(max_workers=None) as executor:
-        results = list(executor.map(lambda link: scrape_link(link, default_sport), links))
-
-    for result in results:
-        pprint.pprint(result)
+        results = [executor.submit(scrape_link, link, default_sport) for link in links]
+        #results = list(executor.submit(lambda link: scrape_link(link, default_sport), links))
+    
+    for future in results:
+        pprint.pprint(future.result())
